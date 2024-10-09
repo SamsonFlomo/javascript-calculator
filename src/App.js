77648c369f076ccddd159a3a3e132575fc2ef707
SamsonFlomo <flomosamson125@gmail.com>
2024-10-09 16:29:29 +0000

    buttonPressed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 import { buttonList } from "./buttons.js";
-import { buttonPressed } from "./utils.js";
 
 function App() {
     const buttons = buttonList;
-
+    const [answer, setAnswer] = useState("0");
+    const [expression, setExpression] = useState(" 0 ");
+    
+    const et = expression.trim();
+    const isOperator = (symbol) => {
+           return /[-+*/]/.test(symbol);
+        };
+    
+    function buttonPressed (symbol) {
+        
+        if (symbol === "clear") {
+            setAnswer("0");
+            setExpression("");
+           } else if (symbol === "negative") {
+              if(answer === "") return;
+              setAnswer(
+                answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+              )
+           } else if (symbol === "percentage") {
+              if(answer === "") return;
+              setAnswer((parseFloat(answer) / 100).toString());
+           } else if (isOperator(symbol)) {
+              setExpression(et + " " + symbol + " ");
+           } else if (symbol === "=") {
+              calculate();
+           } else if (symbol === "0") {
+              if(expression.charAt(0) !== "0") {
+                setExpression( expression + symbol );
+              };
+           } else if (symbol === ".") {
+              const lastNumber = expression.split(/[-+*/]/g).pop();
+              if (lastNumber?.includes(".")) return;
+              setExpression(expression + symbol);
+           } else {
+             if (expression.charAt(0) === "0") {
+               setExpression(expression.split(1) + symbol);
+             } else {
+               setExpression( expression + symbol);
+             };
+           }
+     };
+     
+     const calculate = (input) => {
+        // If the last character is not an operator
+        if(isOperator(et.charAt(et.length -1))) return;
+        // Two operators in a row to use the last operator
+        let dec = 3;
+        input = input.toString();
+        while( dec > 0){
+          const take = input.match(/[-+/*]{2,}/g);
+          console.log("Result: ", take);
+          console.log("Result: ", take[0]);
+          if(input.includes(take[1])) {
+             console.log(true);
+          }
+          dec = dec -1;
+        }
+        
+        console.log("Result: ", input);
+        };
+        
+        calculate("12--3+-4*-*");
+          
+          
   return (
     <div className="App  flex" >
       <h1>Javascript Calculator</h1>
@@ -14,8 +77,8 @@ function App() {
         <div id="brand">SF</div>
       
         <div id="display" className="flex">
-            <div id="answer">24</div>
-            <div id="expression">10 + 14</div>
+            <div id="answer">{answer}</div>
+            <div id="expression">{expression}</div>
         </div>
 
         <div id="buttons">
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,21 @@
-export function buttonPressed (type) {
-        console.log(type);
+import { useState } from "react";
+
+export function buttonPressed (symbol) {
+        
+        
+        switch (symbol) {
+          case "clear":
+            setAnswer("0");
+            setExpression("");
+            break;
+          case "negative":
+            console.log(symbol)
+            break;
+          case "percentage":
+            console.log(symbol)
+            break;
+          default: 
+          console.log("Default Effect");
+        };
     };
+    
